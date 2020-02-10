64340c053acfba26b9326fe6b1f0fbec05a23429
Sundaram Ramaswamy <legends2k@yahoo.com>
2020-02-10 21:11:46 +0530

    [Day 1, Part 2] Use HashSet instead of BTreeSet
    HashSet has constant time insert and find, while BTreeSet has only
    log₂(N) theoretically and worse practically due to cache-misses. This
    is explained in BTreeMap’s documentation:
    https://doc.rust-lang.org/std/collections/struct.BTreeMap.html
    
    * Remove redundant type specifications where type inference would do

--- 2018/day_01/src/main.rs
+++ 2018/day_01/src/main.rs
@@ -1,6 +1,6 @@
 // https://adventofcode.com/2018/day/1
 
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 use std::io::{self, BufRead, Error};
 use std::result::Result;
 
@@ -8,17 +8,16 @@ fn main() {
   let freq: Vec<i32> = io::stdin()
     .lock()
     .lines()
-    .map(|line: Result<String, Error>| -> i32 {
-      line.unwrap_or_default().parse::<i32>().unwrap_or_default()
+    .map(|line: Result<String, Error>| {
+      line.unwrap_or_default().parse().unwrap_or_default()
     })
-    .collect::<Vec<_>>();
+    .collect();
 
   // part 1
-  let result_freq: i32 = freq.iter().sum();
-  println!("{}", result_freq);
+  println!("{}", freq.iter().sum::<i32>());
 
   // part 2
-  let mut resulting_freqs = BTreeSet::new();
+  let mut resulting_freqs = HashSet::new();
   resulting_freqs.insert(0); // 0 is the frequency device starts with
   let mut interrim_freq = 0;
   for f in freq.iter().cycle() {
