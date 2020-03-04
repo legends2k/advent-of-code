340162e1b17b3cbd32260ef8b7981607dab6cd4a
Sundaram Ramaswamy <legends2k@yahoo.com>
2020-03-04 22:59:54 +0530

    [4.2] Find guard with highest asleep frequency/min
    Since finding the most frequent minute of sleepiness is common to both
    strategies move it to a separate function.  Its pair return type seems
    arcane to the uninformed, make a tuple struct for better readability.
    
    Strategy 2 is fairly simple, at least in code.  It’s just finding the
    max element in a collection with a computed key.

--- 2018/day_04/src/main.rs
+++ 2018/day_04/src/main.rs
@@ -6,7 +6,7 @@ type GuardId = u16;
 #[derive(Default)]
 struct GuardSchedule {
   asleep_dur: u16,               // in minutes
-  sleep_interval: Vec<(u8, u8)>, // [a, b)
+  sleep_interval: Vec<(u8, u8)>, // [a, b) in minutes
 }
 
 fn main() {
@@ -37,15 +37,18 @@ fn main() {
 
   // part 1
   strategy1(&guard_schedule);
+
+  // part 2
+  strategy2(&guard_schedule);
 }
 
-fn strategy1(guard_schedule: &HashMap<GuardId, GuardSchedule>) {
-  let most_slept = guard_schedule
-    .iter()
-    .max_by(|a, b| a.1.asleep_dur.cmp(&b.1.asleep_dur))
-    .unwrap();
+// mintue and its occurance frequency pair
+struct MinFreq(u8, u8);
+
+/// Returns the most slept minute with frequency
+fn most_slept_min(shed: &GuardSchedule) -> Option<MinFreq> {
   let mut occurances = [0u8; 60];
-  for i in &most_slept.1.sleep_interval {
+  for i in &shed.sleep_interval {
     (i.0..i.1).for_each(|i| occurances[i as usize] += 1);
   }
   // https://stackoverflow.com/a/58103194/183120
@@ -55,16 +58,56 @@ fn strategy1(guard_schedule: &HashMap<GuardId, GuardSchedule>) {
     .max_by_key(|(_idx, &val)| val) // find max by val
     .map(|(idx, _val)| idx); // but obtain idx as result
   if let Some(idx) = max_idx {
+    return Some(MinFreq(idx as u8, occurances[idx]));
+  }
+  None
+}
+
+fn strategy1(guard_schedule: &HashMap<GuardId, GuardSchedule>) {
+  let most_slept = guard_schedule
+    .iter()
+    .max_by(|a, b| a.1.asleep_dur.cmp(&b.1.asleep_dur))
+    .unwrap();
+  let sleepy_min = most_slept_min(&most_slept.1);
+  if let Some(MinFreq(min, freq)) = sleepy_min {
     println!(
       "Strategy 1 \
        \n  Guard #{} slept the most ({} mins); min {} being \
        the most asleep ({} out of {} times).\n  Result: {}",
       most_slept.0,
       most_slept.1.asleep_dur,
-      idx,
-      occurances[idx],
+      min,
+      freq,
       most_slept.1.sleep_interval.len(),
-      *most_slept.0 * idx as u16
+      *most_slept.0 * min as u16
     );
   }
+}
+
+fn strategy2(guard_schedule: &HashMap<GuardId, GuardSchedule>) {
+  // Calculate max element in guard_schedule manually. Would’ve been nicer if
+  // guard_schedule.iter().max_by_key() worked but that only gives max element
+  // but not the key calculated for sorting; frequency or the max minute.
+  let mut max_freq = 0;
+  let mut sel_min = 0;
+  let mut guard_id = 0;
+  for guard in guard_schedule {
+    let sleepy_min = most_slept_min(guard.1);
+    if let Some(MinFreq(min, freq)) = sleepy_min {
+      if freq > max_freq {
+        max_freq = freq;
+        sel_min = min;
+        guard_id = *guard.0;
+      }
+    }
+  }
+  println!(
+    "Strategy 2 \
+     \n  Guard #{} slept at min {} with highest frequency ({} times) \
+     \n  Result: {}",
+    guard_id,
+    sel_min,
+    max_freq,
+    guard_id * sel_min as u16
+  );
 }
