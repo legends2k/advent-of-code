058a18d39ed34070a6b180500f63d5e093399867
Sundaram Ramaswamy <legends2k@yahoo.com>
2020-03-07 11:00:25 +0530

    [5.1] Optimize by replacing String with Vec<u8>
    * Store bytes instead of chars (4 bytes) to save 3x runtime memory
    * Reserve memory in advance to prevent reallocation and copy

--- 2018/day_05/src/main.rs
+++ 2018/day_05/src/main.rs
@@ -1,19 +1,22 @@
 use std::io::{self, Read};
 
 fn main() {
-  let mut polymer = String::new();
+  // polymer.len() ≤ input file size, reserve to avoid reallocation and copy
+  const FIFTY_KIB: usize = 1024 * 50;
+  // as input is pure ASCII, save 3 bytes per char, use a Vec<u8>, not String
+  let mut polymer: Vec<u8> = Vec::with_capacity(FIFTY_KIB);
   for c in io::stdin().lock().bytes() {
-    if let Ok(ch) = c {
-      let cur = char::from(ch);
+    if let Ok(cur) = c {
+      let cur_ch = char::from(cur);
       // skip newline char at the end
-      if cur.is_ascii_alphabetic() {
-        let prev = match polymer.chars().last() {
-          Some(last) => last,
-          None => '0',
+      if cur_ch.is_ascii_alphabetic() {
+        let prev = match polymer.last() {
+          Some(last) => *last,
+          None => 0,
         };
         if (prev != cur)  // avoid ‘aa’ from passing
-          && ((prev == cur.to_ascii_lowercase())
-            || (prev == cur.to_ascii_uppercase()))
+          && ((char::from(prev) == cur_ch.to_ascii_lowercase())
+          || (char::from(prev) == cur_ch.to_ascii_uppercase()))
         {
           polymer.pop();
         } else {
