95d95b19c0e845f376eee7706a870824348f698a
Sundaram Ramaswamy <legends2k@yahoo.com>
2020-03-07 12:13:26 +0530

    [5.2] Clean-up code for better readability
    * Remove all char bindings and use only u8
    * Convert char on-the-fly, only when needed, at the very end
    * Use filter.contains() instead of filter.iter().any()
    * Use byte literals u8 char constants e.g. b'A'
    * Rename constants

--- 2018/day_05/src/main.rs
+++ 2018/day_05/src/main.rs
@@ -1,46 +1,44 @@
 use std::io::{self, Read};
 
-fn fold(cur_ch: char, polymer: &mut Vec<u8>, filter: &[u8]) {
-  let cur = cur_ch as u8;
-  if filter.iter().any(|&f| f == cur) {
-    return;
-  }
-  let prev = match polymer.last() {
-    Some(last) => *last,
-    None => 0, // NUL
-  };
-  if (prev != cur)  // avoid ‘aa’ from passing
-    && ((char::from(prev) == cur_ch.to_ascii_lowercase())
-    || (char::from(prev) == cur_ch.to_ascii_uppercase()))
-  {
-    polymer.pop();
-  } else {
-    polymer.push(cur);
+fn fold(cur: u8, polymer: &mut Vec<u8>, filter: &[u8]) {
+  if !filter.contains(&cur) {
+    let prev = match polymer.last() {
+      Some(last) => *last,
+      None => 0, // NUL
+    };
+    if (prev != cur)  // avoid ‘aa’ from passing
+      && ((char::from(prev) == char::from(cur).to_ascii_lowercase())
+      ||  (char::from(prev) == char::from(cur).to_ascii_uppercase()))
+    {
+      polymer.pop();
+    } else {
+      polymer.push(cur);
+    }
   }
 }
 
 fn main() {
-  // polymer.len() ≤ input file size, reserve to avoid reallocation and copy
+  // polymer.len() ≤ input file size, preallocate to avoid reallocation and copy
   const FIFTY_KIB: usize = 1024 * 50;
-  const LETTER_COUNT: usize = 26;
-  const FIRST: u8 = 'A' as u8;
-  const LAST: u8 = 'Z' as u8;
-  const UPPER_LOWER_DIFF: u8 = 32;
-  // as input is pure ASCII, save 3 bytes per char, use a Vec<u8>, not String
+  const FIRST: u8 = b'A';
+  const LAST: u8 = b'Z';
+  const LETTER_COUNT: usize = (LAST - FIRST + 1) as usize;
+  const CASE_DIFF: u8 = 32;
+
+  // input is pure ASCII so use a Vec<u8>, not String, save 3 bytes per char
   let mut polymer: Vec<u8> = Vec::with_capacity(FIFTY_KIB);
   let mut filtered_polymers: Vec<Vec<u8>> = vec![polymer.clone(); LETTER_COUNT];
 
   for c in io::stdin().lock().bytes() {
     if let Ok(cur) = c {
-      let cur_ch = char::from(cur);
       // skip newline char at the end
-      if cur_ch.is_ascii_alphabetic() {
-        fold(cur_ch, &mut polymer, &[]); // part 1
+      if char::from(cur).is_ascii_alphabetic() {
+        fold(cur, &mut polymer, &[]); // part 1
 
         // part 2
         for (idx, letter) in (FIRST..=LAST).enumerate() {
-          let filtered_letters = [letter, letter + UPPER_LOWER_DIFF];
-          fold(cur_ch, &mut filtered_polymers[idx], &filtered_letters);
+          let filter = [letter, letter + CASE_DIFF];
+          fold(cur, &mut filtered_polymers[idx], &filter);
         }
       }
     }
