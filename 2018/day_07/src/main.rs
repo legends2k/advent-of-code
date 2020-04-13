4dfb42dbd02a74377f98b834bf8c052dd0011efd
Sundaram Ramaswamy <legends2k@yahoo.com>
2020-04-13 20:59:53 +0530

    [7.1] Fix to work with inputs lesser than 26 values
    Use a state enum to denote uninitialized tasks in case input has fewer
    than 26 tasks e.g. sample input given in the puzzle description.
    
    Can’t use is_complete: bool as a container of Tasks might have unused
    slots -- Uninitialized tasks -- which should be ignored.

--- 2018/day_07/src/main.rs
+++ 2018/day_07/src/main.rs
@@ -1,9 +1,16 @@
 use std::char::ParseCharError;
 use std::io::{self, BufRead};
 
+#[derive(Debug, PartialEq)]
+enum Status {
+  Uninitialized,
+  Waiting,
+  Complete,
+}
+
 #[derive(Debug)]
 struct Task {
-  is_complete: bool,
+  status: Status,
   awaiting: u8,
   // Inversion of control: dependants instead of dependencies
   // https://en.wikipedia.org/wiki/Inversion_of_control
@@ -14,15 +21,22 @@ struct Task {
 impl Default for Task {
   fn default() -> Self {
     Task {
-      is_complete: false,
+      status: Status::Uninitialized,
       awaiting: 0,
-      dependants: Vec::<u8>::with_capacity(10),
+      dependants: Vec::<u8>::with_capacity(5),
     }
   }
 }
 
+impl Task {
+  fn is_waiting(&self) -> bool {
+    self.status == Status::Waiting
+  }
+}
+
 fn perform(task_id: u8, tasks: &mut [Task]) {
-  tasks[task_id as usize].is_complete = true;
+  debug_assert!(tasks[task_id as usize].is_waiting());
+  tasks[task_id as usize].status = Status::Complete;
   for &d in &tasks[task_id as usize].dependants {
     tasks[d as usize].awaiting -= 1;
   }
@@ -45,25 +59,33 @@ fn main() -> Result<(), ParseCharError> {
     }
     let dependency = line[5..6].parse::<char>()? as u8 - b'A';
     let this_task = line[36..37].parse::<char>()? as u8 - b'A';
+    if dependency >= 26 || this_task >= 26 {
+      panic!("Input has more than 26 steps!");
+    }
+    tasks[this_task as usize].status = Status::Waiting;
     tasks[this_task as usize].awaiting += 1;
+    // tasks with no dependency don’t get their own line in the input
+    // so set its status too
+    tasks[dependency as usize].status = Status::Waiting;
     if !tasks[dependency as usize].dependants.contains(&this_task) {
       tasks[dependency as usize].dependants.push(this_task);
     }
   }
 
+  // part 1 -- task sequencer
   let mut task_order = String::with_capacity(26);
-  while let Some(_) = tasks.iter().find(|t| !t.is_complete) {
+  while tasks.iter().any(|t| t.is_waiting()) {
     let mut available_tasks: Vec<u8> = tasks
       .iter()
       .enumerate()
-      .filter(|(_id, task)| !task.is_complete && task.awaiting == 0)
+      .filter(|(_id, task)| task.is_waiting() && task.awaiting == 0)
       .map(|(id, _task)| id as u8)
       .collect();
     available_tasks.sort_unstable();
     perform(available_tasks[0], &mut tasks);
     task_order.push(char::from(b'A' + available_tasks[0]));
   }
-  println!("{}", task_order);
+  println!("Task sequence: {}", task_order);
 
   Ok(())
 }
