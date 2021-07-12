5484d0687bc8bdba2a68023560926f602dd660c5
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-07-12 10:30:42 +0530

    [11.1] Compute 3x3 square with maximum power
    Brute force approach but slightly better as it’s cache-friendly.
    Instead of computing sum of every 3x3 square, compute horizontal and
    vertical sums separately and then sum horizontal and vertical as final
    step.

--- 2018/day_11/src/main.rs
+++ 2018/day_11/src/main.rs
@@ -13,5 +13,37 @@ fn main() {
       eprintln!("Failed to convert SERIAL into a number: {}", err);
       process::exit(1);
     });
-  println!("{}", serial);
+
+  const SIZE: i32 = 300;
+  let mut cells = [0; (SIZE * SIZE) as usize];
+  for row in 0..SIZE {
+    for col in 0..SIZE {
+      let rack = col + 11;
+      let t = (rack * (row + 1) + serial) * rack;
+      cells[(row * SIZE + col) as usize] = ((t / 100) % 10) - 5;
+    }
+  }
+
+  // compute cumulative powers in cache-friendly way
+  // compute cumulative X powers
+  for row in 0..(SIZE - 2) {
+    for col in 0..(SIZE - 2) {
+      let idx = (row * SIZE + col) as usize;
+      cells[idx] += cells[idx + 1] + cells[idx + 2];
+    }
+  }
+
+  // compute cumulative Y powers
+  for col in 0..(SIZE - 2) {
+    for row in 0..(SIZE - 2) {
+      let idx = (row * SIZE + col) as usize;
+      cells[idx] += cells[idx + SIZE as usize] + cells[idx + SIZE as usize * 2];
+    }
+  }
+
+  if let Some((i, _)) = cells.iter().enumerate().max_by_key(|(_, &x)| x) {
+    let row = i as i32 / SIZE;
+    let col = i as i32 - row * SIZE;
+    println!("Maximum powered square at {},{}", col + 1, row + 1);
+  }
 }
