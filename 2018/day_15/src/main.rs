35739ada569104fa94d3cb9b014717aa8a5a6cf2
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-08-05 17:18:38 +0530

    [15.1] Complete and verify path finding
    * Fix path finding next_step function
      - Don’t break when a target point is reached; there might be other
        targets reached with the same distance that come earlier in
        reading-order which have to be chosen
    * Implement move_fighter to reposition the unit on map
    * Implement Fighter::target to deduce enemy unit covered by this
    * Remove Fighter::id as it isn’t used since the collection is sorted
      after every round
    * Learn about matches! macro with clippy’s help
    * Implement Partial{,Ord} for Point and Fighter for terse < comparisons
    * Verify input/sample_3’s turns against the puzzle description

--- 2018/day_15/src/main.rs
+++ 2018/day_15/src/main.rs
@@ -1,5 +1,6 @@
 use core::fmt;
 use std::{
+  cmp::Ordering,
   error::Error,
   fmt::Display,
   io::{stdin, BufRead},
@@ -9,42 +10,83 @@ use std::{
 #[derive(Copy, Clone, Debug, Default, PartialEq, Eq)]
 struct Point(u8, u8);
 
+impl Ord for Point {
+  fn cmp(&self, other: &Self) -> Ordering {
+    match self.1 != other.1 {
+      true => self.1.cmp(&other.1),
+      false => self.0.cmp(&other.0),
+    }
+  }
+}
+
+impl PartialOrd for Point {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
+}
+
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum FighterKind {
   Elf,
   Goblin,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq)]
 struct Fighter {
-  id: u8,
   kind: FighterKind,
   pos: Point,
+}
+
+impl Ord for Fighter {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self.pos.cmp(&other.pos)
+  }
+}
+
+impl PartialOrd for Fighter {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.pos.cmp(&other.pos))
+  }
+}
+
+impl Fighter {
+  fn target(&self, map: &Map) -> Option<Point> {
+    [
+      Point(self.pos.0, self.pos.1 - 1),
+      Point(self.pos.0 - 1, self.pos.1),
+      Point(self.pos.0 + 1, self.pos.1),
+      Point(self.pos.0, self.pos.1 + 1),
+    ]
+    .iter()
+    .find(|&&pt| match map.cell(pt) {
+      Some(Cell::Occupied { kind, .. }) if kind != self.kind => true,
+      _ => false,
+    })
+    .map(|&pt| pt)
+  }
 }
 
 #[derive(Copy, Clone)]
 enum Cell {
   Wall,
   Vacant { previous: Point, dist: u16 },
-  Occupied { kind: FighterKind, id: u8 },
+  Occupied { kind: FighterKind },
 }
 
 impl Cell {
   const MAX: u16 = u16::MAX;
 
   fn is_vacant(&self) -> bool {
-    match self {
-      Cell::Vacant { .. } => true,
-      _ => false,
-    }
+    matches!(self, Cell::Vacant { .. })
   }
 }
 
 impl fmt::Display for Cell {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    // https://stackoverflow.com/q/41390457/183120
     let symbol = match self {
       Cell::Wall => b'#',
+      // Destructuring structs when matching
+      // https://stackoverflow.com/q/41390457/183120
       Cell::Vacant { .. } => b'.',
       Cell::Occupied {
         kind: FighterKind::Elf,
@@ -74,10 +116,6 @@ struct Map {
 }
 
 impl Map {
-  fn idx_to_point(&self, idx: usize) -> Point {
-    Point((idx / self.width) as u8, (idx % self.width) as u8)
-  }
-
   fn point_to_idx(&self, pt: Point) -> usize {
     self.width * pt.1 as usize + pt.0 as usize
   }
@@ -114,24 +152,21 @@ impl Map {
       .iter()
       .filter(|&fighter| fighter.kind == enemy_kind)
       .flat_map(|fighter| {
-        let neighbours = [
+        [
           Point(fighter.pos.0, fighter.pos.1 - 1),
           Point(fighter.pos.0 - 1, fighter.pos.1),
           Point(fighter.pos.0 + 1, fighter.pos.1),
           Point(fighter.pos.0, fighter.pos.1 + 1),
-        ];
-        let mut targets = Vec::<Point>::with_capacity(4);
-        for n in neighbours {
-          if self.is_vacant(n) {
-            targets.push(n);
-          }
-        }
-        targets
+        ]
+        .iter()
+        .filter(|&&pt| self.is_vacant(pt))
+        .map(|&pt| pt)
+        .collect::<Vec<_>>()
       })
       .collect()
   }
 
-  // reset distances stored in vacant cells to default
+  /** Reset distances stored in vacant cells to default */
   fn clear(&mut self) {
     self
       .layout
@@ -150,7 +185,11 @@ impl Map {
     self.layout[idx] = cell;
   }
 
-  fn next_step(&mut self, src: Point, dsts: &Vec<Point>) -> Option<Point> {
+  /** Deduce possible next step for unit at `src` */
+  fn next_step(&mut self, src: Point, dsts: &[Point]) -> Option<Point> {
+    if dsts.is_empty() {
+      return None;
+    }
     self.clear();
     // loop until all vacancies are visited or one of |dsts| is reached
     let mut visiting = Vec::<(Point, Point)>::with_capacity(256);
@@ -161,16 +200,17 @@ impl Map {
     to_visit.push((Point(src.0 - 1, src.1), src));
     to_visit.push((Point(src.0, src.1 - 1), src));
     let mut cur_dist = 0;
-    let mut found_dst: Option<Point> = None;
+    let mut final_dst: Option<Point> = None;
 
-    'outer: while !to_visit.is_empty() {
+    while !to_visit.is_empty() {
       mem::swap(&mut visiting, &mut to_visit);
       to_visit.clear();
       cur_dist += 1;
       while let Some((pt, from)) = visiting.pop() {
         let cell = self.cell(pt); // proceed only if cell is vacant
         if let Some(Cell::Vacant { previous, dist }) = cell {
-          if dist > cur_dist {
+          // if path shorter or if same distance tie break with previous point
+          if dist > cur_dist || ((dist == cur_dist) && (previous > from)) {
             self.set(
               pt,
               Cell::Vacant {
@@ -178,10 +218,31 @@ impl Map {
                 dist: cur_dist,
               },
             );
-            if let Some(found_idx) = dsts.iter().position(|&p| p == pt) {
-              // |cur_dist| ascends so closest distance has been found; break
-              found_dst = Some(dsts[found_idx]);
-              break 'outer;
+            if dsts.iter().any(|&p| p == pt) {
+              final_dst = match final_dst {
+                None => {
+                  println!("None: Setting final dst: {:?}", pt);
+                  Some(pt)
+                }
+                Some(old_dst) => match self.cell(old_dst) {
+                  // Destructuring structs and ignoring fields (not positional)
+                  // https://stackoverflow.com/a/38031302/183120
+                  Some(Cell::Vacant { dist, .. }) => {
+                    match (dist > cur_dist)
+                      || (dist == cur_dist && pt < old_dst)
+                    {
+                      true => {
+                        println!(
+                          "Some: Setting final dst: {:?}; old, new dist: {}, {}",
+                          pt, dist, cur_dist);
+                        Some(pt)
+                      }
+                      false => final_dst,
+                    }
+                  }
+                  _ => panic!("Invalid state"),
+                },
+              };
             }
             to_visit.push((Point(pt.0, pt.1 + 1), pt));
             to_visit.push((Point(pt.0 + 1, pt.1), pt));
@@ -193,22 +254,34 @@ impl Map {
     }
 
     // if found a path, backtrack and choose the optimal next step
-    match found_dst {
+    match final_dst {
       Some(mut pt) => {
         loop {
-          if let Some(Cell::Vacant { previous, .. }) = self.cell(pt) {
-            match self.is_vacant(previous) {
-              true => pt = previous,
-              false => break,
+          match self.cell(pt) {
+            Some(Cell::Vacant { previous, .. }) if self.is_vacant(previous) => {
+              pt = previous
             }
-          } else {
-            panic!("Unexpected state")
+            Some(Cell::Vacant { .. }) => break,
+            _ => panic!("Unexpected state"),
           }
         }
         Some(pt)
       }
       None => None,
     }
+  }
+
+  fn move_fighter(&mut self, fighter_idx: usize, pt: Point) {
+    let old_idx = self.point_to_idx(self.fighters[fighter_idx].pos);
+    self.layout[old_idx] = Cell::Vacant {
+      previous: Point::default(),
+      dist: Cell::MAX,
+    };
+    self.fighters[fighter_idx].pos = pt;
+    let new_idx = self.point_to_idx(pt);
+    self.layout[new_idx] = Cell::Occupied {
+      kind: self.fighters[fighter_idx].kind,
+    };
   }
 }
 
@@ -236,37 +309,30 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut digest: Vec<Cell> = line
       .bytes()
       .enumerate()
-      .map(|(idx, symbol)| {
-        let id = fighters.len() as u8;
-        match symbol {
-          b'#' => Cell::Wall,
-          b'E' => {
-            fighters.push(Fighter {
-              id,
-              kind: FighterKind::Elf,
-              pos: Point(idx as u8, height as u8),
-            });
-            Cell::Occupied {
-              kind: FighterKind::Elf,
-              id,
-            }
+      .map(|(idx, symbol)| match symbol {
+        b'#' => Cell::Wall,
+        b'E' => {
+          fighters.push(Fighter {
+            kind: FighterKind::Elf,
+            pos: Point(idx as u8, height as u8),
+          });
+          Cell::Occupied {
+            kind: FighterKind::Elf,
           }
-          b'G' => {
-            fighters.push(Fighter {
-              id,
-              kind: FighterKind::Goblin,
-              pos: Point(idx as u8, height as u8),
-            });
-            Cell::Occupied {
-              kind: FighterKind::Goblin,
-              id,
-            }
-          }
-          _ => Cell::Vacant {
-            previous: Point::default(),
-            dist: Cell::MAX,
-          },
         }
+        b'G' => {
+          fighters.push(Fighter {
+            kind: FighterKind::Goblin,
+            pos: Point(idx as u8, height as u8),
+          });
+          Cell::Occupied {
+            kind: FighterKind::Goblin,
+          }
+        }
+        _ => Cell::Vacant {
+          previous: Point::default(),
+          dist: Cell::MAX,
+        },
       })
       .collect();
     layout.append(&mut digest);
@@ -280,13 +346,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     width,
     height,
   };
-  println!("{}", map);
 
-  let test_pt = Point(1, 1);
-  let targets = map.targets(test_pt);
-  println!("{:?}", targets);
-  if let Some(pt) = map.next_step(test_pt, &targets) {
-    println!("{:?}", pt);
+  for _ in 0..3 {
+    // fix turn order amongst fighters
+    map.fighters.sort();
+    for idx in 0..map.fighters.len() {
+      // attack or move?
+      if let Some(_) = map.fighters[idx].target(&map) {
+      } else {
+        let targets = map.targets(map.fighters[idx].pos);
+        if let Some(pt) = map.next_step(map.fighters[idx].pos, &targets) {
+          map.move_fighter(idx, pt);
+          println!("{}", map);
+        }
+      }
+    }
   }
 
   Ok(())
