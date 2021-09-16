795c54cec6eb361f372c5365a0fd0cd3663504e4
Sundaram Ramaswamy <legends2k@yahoo.com>
2021-09-16 13:59:03 +0530

    [20] Move preprocessor to its own function
    * Remove `unwrap` and check with `ok_or`
      - This needs iteration with for instead of for_each as the latter
        expects a closure returning `()`
    * Make allocation more readable

--- 2018/day_20/src/main.rs
+++ 2018/day_20/src/main.rs
@@ -4,47 +4,52 @@ use std::io::{self, Read};
 #[derive(Copy, Clone)]
 struct Point(i32, i32);
 
-fn main() -> Result<(), Box<dyn Error>> {
-  // allocate 15 KiB memory
-  let mut expr = String::with_capacity(15_360);
-  // drop the trailing linefeed ‘\n’
-  let n = io::stdin().read_to_string(&mut expr)? - 1;
-  expr.pop();
+fn compute_dims(input: &str) -> Result<(Point, Point), Box<dyn Error>> {
   let mut fork_points = Vec::with_capacity(128);
   let mut pos = Point(0, 0);
   let (mut min, mut max) =
     (Point(i32::MAX, i32::MAX), Point(-i32::MAX, -i32::MAX));
-  expr
+  for c in input
     .bytes()
     .skip_while(|&c| c != b'^')
     .skip(1)
     .take_while(|&c| c != b'$')
-    .for_each(|c| {
-      match c {
-        b'N' => pos.1 += 2,
-        b'S' => pos.1 -= 2,
-        b'W' => pos.0 -= 2,
-        b'E' => pos.0 += 2,
-        b'(' => fork_points.push(pos),
-        b'|' => pos = *fork_points.last().unwrap(),
-        b')' => {
-          fork_points.pop();
-        }
-        _ => unreachable!(),
+  {
+    match c {
+      b'N' => pos.1 += 2,
+      b'E' => pos.0 += 2,
+      b'W' => pos.0 -= 2,
+      b'S' => pos.1 -= 2,
+      b'(' => fork_points.push(pos),
+      b'|' => pos = *fork_points.last().ok_or("Invalid input")?,
+      b')' => {
+        fork_points.pop();
       }
-      min.0 = min.0.min(pos.0);
-      max.0 = max.0.max(pos.0);
-      min.1 = min.1.min(pos.1);
-      max.1 = max.1.max(pos.1);
-    });
+      _ => unreachable!(),
+    }
+    min.0 = min.0.min(pos.0);
+    max.0 = max.0.max(pos.0);
+    min.1 = min.1.min(pos.1);
+    max.1 = max.1.max(pos.1);
+  }
   // expand by 1 on all sides for the walls
   min.0 -= 1;
   min.1 -= 1;
   max.0 += 1;
   max.1 += 1;
+  // + 1 as we want stops and not spans
   let dim_x = max.0 - min.0 + 1;
   let dim_y = max.1 - min.1 + 1;
-  println!("Map size: {} × {}", dim_x, dim_y);
-  println!("Pos: ({}, {})", -min.0, max.1);
+  Ok((Point(dim_x, dim_y), Point(-min.0, max.1)))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let mut input = String::with_capacity(15 * 1024);
+  // drop the trailing linefeed ‘\n’
+  io::stdin().read_to_string(&mut input)?;
+  input.pop();
+
+  compute_dims(&input)?;
+
   Ok(())
 }
