c437e18b947204e30d3cdf20bcdc8495955c1ae7
Sundaram Ramaswamy <legends2k@yahoo.com>
2022-03-28 09:36:31 +0530

    [22.2] Initial attempt at solution
    Storing only one tool's cost won't work since it is over-written by the
    alternate tool with same cost.  However, the other tool is more
    favourable down the line.  Storing both tools' cost might work.

---
+++ 2018/day_22/notes.md
@@ -0,0 +1,16 @@
+Allowed: One or none
+Hand: torch
+Movement cost:  1 min
+Switching cost: 7 min
+
+Mouth & Target always rocky (need torch)
+
+| Region | Symbol | Tool          |
+|--------|--------|---------------|
+| Rocky  |   `.`  | Gear or Torch |
+| Wet    |   `=`  | Gear or NONE  |
+| Narrow |   `|`  | Torch or NONE |
+
+Adjacent move allowed only if equipment is permitted by type i.e. you’ve to switch before you leave.  Favour move over switch in general as latter costs 7 times more.
+
+Cells with negative coordinates aren’t walkable.
--- 2018/day_22/src/main.rs
+++ 2018/day_22/src/main.rs
@@ -1,9 +1,28 @@
-use std::collections::HashMap;
-use std::error::Error;
-use std::io::stdin;
+use std::{
+  collections::{HashMap, VecDeque},
+  error::Error,
+  fmt::{self, Debug, Formatter},
+  io::stdin,
+  mem,
+};
 
 #[derive(Hash, PartialEq, Eq, Copy, Clone, Debug)]
 struct Point(i32, i32);
+
+impl Point {
+  fn is_valid(&self) -> bool {
+    (self.0 >= 0) && (self.1 >= 0)
+  }
+}
+
+#[repr(u8)]
+#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+enum Tool {
+  None,
+  Gear,
+  Torch,
+  Unkown,
+}
 
 #[repr(u8)]
 #[derive(Copy, Clone, Debug)]
@@ -13,17 +32,72 @@ enum RegionType {
   Narrow,
 }
 
+impl RegionType {
+  fn get(erosion: u64) -> Self {
+    match erosion % 3 {
+      0 => RegionType::Rocky,
+      1 => RegionType::Wet,
+      2 => RegionType::Narrow,
+      _ => unreachable!(),
+    }
+  }
+
+  fn is_allowed(self, t: Tool) -> bool {
+    matches!(
+      (self, t),
+      (RegionType::Rocky, Tool::Gear)
+        | (RegionType::Rocky, Tool::Torch)
+        | (RegionType::Wet, Tool::Gear)
+        | (RegionType::Wet, Tool::None)
+        | (RegionType::Narrow, Tool::Torch)
+        | (RegionType::Narrow, Tool::None)
+    )
+  }
+
+  fn get_alternate(self, t: Tool) -> Tool {
+    match (self, t) {
+      (RegionType::Rocky, Tool::Gear) => Tool::Torch,
+      (RegionType::Rocky, Tool::Torch) => Tool::Gear,
+      (RegionType::Wet, Tool::Gear) => Tool::None,
+      (RegionType::Wet, Tool::None) => Tool::Gear,
+      (RegionType::Narrow, Tool::Torch) => Tool::None,
+      (RegionType::Narrow, Tool::None) => Tool::Torch,
+      _ => unreachable!(),
+    }
+  }
+}
+
+#[derive(Clone)]
 struct Region {
   erosion: u64,
-  cost: [i32; 2], // per-tool cost
+  // negative implies yet-to-reach
+  cost: i32, // per-tool cost
+  tool: Tool,
+  from: Point,
 }
 
 impl Region {
   fn new(erosion: u64) -> Self {
     Region {
       erosion,
-      cost: [i32::MIN; 2],
+      cost: i32::MIN,
+      tool: Tool::Unkown,
+      from: Point(0, 0),
     }
+  }
+}
+
+impl Debug for Region {
+  fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+    write!(
+      f,
+      "{:?} \t {:?} \t {:?} \t {}",
+      RegionType::get(self.erosion),
+      self.from,
+      self.tool,
+      self.cost
+    )?;
+    Ok(())
   }
 }
 
@@ -68,33 +142,110 @@ impl Map {
     }
   }
 
-  fn get_type(&mut self, pt: Point) -> RegionType {
-    let erosion = self.get_erosion(pt);
-    match erosion % 3 {
-      0 => RegionType::Rocky,
-      1 => RegionType::Wet,
-      2 => RegionType::Narrow,
-      _ => unreachable!(),
-    }
-  }
-
   fn risk_level(&mut self) -> u32 {
     let width = self.target.0;
     let height = self.target.1;
     (0..=width)
       .flat_map(|x| (0..=height).map(move |y| Point(x, y)))
-      .map(|pt| self.get_type(pt) as u32)
+      .map(|pt| RegionType::get(self.get_erosion(pt)) as u32)
       .sum()
+  }
+
+  fn get_region(&mut self, pos: Point) -> Option<&mut Region> {
+    match (pos.is_valid(), self.cells.contains_key(&pos)) {
+      (true, true) => self.cells.get_mut(&pos),
+      (true, false) => {
+        self.get_erosion(pos); // this inserts <Point, Region> into |self.cells|
+        self.cells.get_mut(&pos)
+      }
+      _ => None,
+    }
   }
 }
 
-fn shortest_path_cost(m: &Map) -> u32 {
-  // Go to adjacent cell with least cost
-  //   Put cost-to-reach in each cell
+fn shortest_path(m: &mut Map, target: Point) -> i32 {
+  // Visit every adjacent cell
+  //   Put cost-to-reach
   // Keep going in all 4 directions
-  // Target reached
+  // Reach target
   // Drop yet-to-visit cells with higher cost
   // Complete all yet-to-visit cells with lower cost
+
+  // Every node X has adjacent nodes Y1, Y2, ….  Every Y has two edges from X:
+  //   1. With tool A
+  //   2. With tool B
+  // Shortest path from X to Y depends on the equiped tool when arriving at X.
+  // However, once the cost and tool to arrive at X is known, the shortest edge
+  // is eihter (1) or (2).  If in future a shorter route to X (with a different
+  // tool) is found, we anyway move X to Open and revisit its descendants.
+
+  let mut to_visit = VecDeque::<Point>::with_capacity(256);
+  let mut visiting = VecDeque::<Point>::with_capacity(256);
+  let mouth = m.get_region(Point(0, 0)).unwrap();
+  mouth.tool = Tool::Torch;
+  mouth.cost = 0;
+  to_visit.push_back(Point(0, 0));
+  let mut reached = false;
+  let mut target_cost = i32::MIN;
+
+  while !to_visit.is_empty() {
+    mem::swap(&mut visiting, &mut to_visit);
+
+    while let Some(pos) = visiting.pop_front() {
+      let region = m.get_region(pos).unwrap().clone();
+      // only pursue paths less costlier than current target cost if reached
+      if reached && (region.cost >= target_cost) {
+        continue;
+      }
+      let adjs = [
+        Point(pos.0 - 1, pos.1),
+        Point(pos.0 + 1, pos.1),
+        Point(pos.0, pos.1 - 1),
+        Point(pos.0, pos.1 + 1),
+      ];
+      for adj_pos in adjs {
+        if let Some(adj_region) = m.get_region(adj_pos) {
+          let (mut cost, mut tool) =
+            match RegionType::get(adj_region.erosion).is_allowed(region.tool) {
+              true => ((region.cost + 1), region.tool),
+              false => (
+                (region.cost + 1 + 7),
+                RegionType::get(region.erosion).get_alternate(region.tool),
+              ),
+            };
+          // if target, include switching cost if not holding torch already
+          if (adj_pos == target) && (adj_region.tool != Tool::Torch) {
+            tool = Tool::Torch;
+            cost += 7;
+          }
+          if pos == Point(4, 1) && adj_pos == Point(4, 2) {
+            println!("{} -- {} ({:?})", adj_region.cost, cost, tool);
+          }
+          // update if already visited and has a lesser cost
+          if (adj_region.cost >= 0) && (adj_region.cost < cost) {
+            continue;
+          }
+          if pos == Point(4, 1) && adj_pos == Point(4, 2) {
+            println!("here!");
+          }
+          // (adj_region.cost > 0) && (adj_region.cost > cost)
+          // Not worrying about: already reached with greater cost as this
+          // iteration will anyway start new follow-ups.
+          adj_region.cost = cost;
+          adj_region.tool = tool;
+          adj_region.from = pos;
+          if adj_pos == target {
+            target_cost = cost;
+            reached = true;
+          } else if !reached || (cost < target_cost) {
+            // pursue if target unreached or this cell’s cost is lesser
+            to_visit.push_back(adj_pos);
+          }
+        }
+      }
+    }
+  }
+  target_cost
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -120,6 +271,22 @@ fn main() -> Result<(), Box<dyn Error>> {
   let mut m = Map::new(depth, target_pos);
   // Part 1: print risk level of rectangle from cave mouth to target
   println!("Risk level: {}", m.risk_level());
+
+  println!(
+    "Fastest path to target: {}",
+    shortest_path(&mut m, target_pos)
+  );
+  dbg!(m.get_region(Point(1, 1)).unwrap());
+  dbg!(m.get_region(Point(2, 1)).unwrap());
+  dbg!(m.get_region(Point(3, 1)).unwrap());
+  dbg!(m.get_region(Point(4, 1)).unwrap());
+  dbg!(m.get_region(Point(4, 2)).unwrap());
+  dbg!(m.get_region(Point(4, 3)).unwrap());
+  dbg!(m.get_region(Point(4, 4)).unwrap());
+  dbg!(m.get_region(Point(4, 5)).unwrap());
+  dbg!(m.get_region(Point(4, 6)).unwrap());
+  dbg!(m.get_region(Point(4, 7)).unwrap());
+  dbg!(m.get_region(Point(4, 8)).unwrap());
 
   Ok(())
 }
