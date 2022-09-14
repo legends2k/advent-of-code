bb82f0d4c9011b8afb81236ad388e1390d1820e0
Sundaram Ramaswamy <legends2k@yahoo.com>
2022-09-14 10:07:09 +0530

    [24] Add data structures for Army and Group
    * Convert attack names into Attack type (u8)
      - Represent Attack with u8 bits as max attacks are < 8 for input
    * Read all of STDIN to a string and pass to PEG parser
    * Drop extern crate statements as Rust 2018 makes them redundant [1]
    
    [1]: https://stackoverflow.com/q/64623568/183120

--- 2018/day_24/src/main.rs
+++ 2018/day_24/src/main.rs
@@ -1,27 +1,59 @@
-extern crate pest;
-#[macro_use]
-extern crate pest_derive;
-
 use pest::Parser;
+use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "input.pest"]
 pub struct InputParser;
 
-fn main() {
-  let input = InputParser::parse(
-    Rule::file,
-    "Immune System:
-5711 units each with 6662 hit points (immune to fire; weak to slashing) with an attack that does 9 bludgeoning damage at initiative 14
-2108 units each with 8185 hit points (weak to radiation, bludgeoning) with an attack that does 36 slashing damage at initiative 13
+use std::{
+  collections::HashMap,
+  error::Error,
+  io::{self, Read},
+};
 
-Infection:
-1492 units each with 47899 hit points (weak to fire, slashing; immune to cold) with an attack that does 56 bludgeoning damage at initiative 15
-8714 units each with 7890 hit points with an attack that does 1 cold damage at initiative 3",
-  ).expect("Invalid input").next().unwrap();
+struct Attack(u8);
+
+struct Group {
+  units: u32,
+  hits: u32,
+  attacks: u32,
+  attack: Attack,
+  initiative: u8,
+  immunity: u8,
+  weakness: u8,
+}
+
+impl Group {
+  fn effective_power(&self) -> u32 {
+    self.units * self.attacks
+  }
+
+  fn is_empty(&self) -> bool {
+    self.units <= 0
+  }
+}
+
+fn to_flag<'a>(
+  attack: &'a str,
+  attack_to_flag: &mut HashMap<&'a str, u8>,
+) -> Result<u8, Box<dyn Error>> {
+  let n = attack_to_flag.len();
+  assert!(n <= 7);
+  Ok(*attack_to_flag.entry(attack).or_insert(1u8 << n))
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+  let mut input_str = String::new();
+  let mut stdin = io::stdin();
+  stdin.read_to_string(&mut input_str)?;
+  let input = InputParser::parse(Rule::file, &input_str)
+    .expect("Invalid input")
+    .next()
+    .unwrap();
 
   let mut current_army: &str;
   let mut current_group: u32 = 0;
+  let mut attack_to_flag: HashMap<&str, u8> = HashMap::new();
   for line in input.into_inner() {
     match line.as_rule() {
       Rule::army_name => {
@@ -67,14 +99,27 @@ Infection:
         println!("  Group {}", current_group);
         println!("    Units: {}", counts[0]);
         println!("    Hits: {}", counts[1]);
-        println!("    {}: {}", attack, counts[2]);
+        println!(
+          "    {:?}: {}",
+          to_flag(attack, &mut attack_to_flag)?,
+          counts[2]
+        );
         println!("    Initiative: {}", counts[3]);
-        println!("    Immunities: {:?}", immunities);
-        println!("    Weaknesses: {:?}", weaknesses);
+        print!("    Immunities: [");
+        for i in &immunities {
+          print!("{}, ", to_flag(i, &mut attack_to_flag)?);
+        }
+        print!("]\n    Weaknesses: [");
+        for w in &weaknesses {
+          print!("{}, ", to_flag(w, &mut attack_to_flag)?);
+        }
+        println!("]");
         current_group += 1;
       }
       Rule::EOI => (),
       _ => unreachable!(),
     }
   }
+  println!("{:?}", attack_to_flag);
+  Ok(())
 }
