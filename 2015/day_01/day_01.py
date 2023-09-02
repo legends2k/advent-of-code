64c80533272040412099bda12734298dcb8d0abe
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-09-03 00:30:52 +0530

    [2015.1] [Python] Solve both parts
    Trying to be more conversant with Python with AoC 2015.

---
+++ 2015/day_01/day_01.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+import functools
+import array
+
+
+input_ = b''.join((line.rstrip('\n').encode() for line in sys.stdin))
+signs = array.array('b', [1, -1])
+
+# Part 1
+floor = functools.reduce(lambda sum, i: signs[i - ord(b'(')] + sum, input_, 0)
+print(floor)
+
+# Part 2
+floor = 0
+for i, c in enumerate(input_, 1):
+  floor += signs[c - ord(b'(')]
+  if floor == -1:
+    print(i)
+    break
