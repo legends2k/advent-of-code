0a86ea58d7389c656da6b2c2796527c398ab1b55
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-09-04 16:45:37 +0530

    [2015.2.2] [Python] Compute ribbon length
    * Convert genexp into listcomp
      - We don’t want the input to be consumed by part 1 solution
    * Used starred expression to get two minimum sides

--- 2015/day_02/day_02.py
+++ 2015/day_02/day_02.py
@@ -9,13 +9,22 @@ class Box:
     w = int(w)
     h = int(h)
     self.areas = (l * w, w * h, h * l)
+    self.volume = l * w * h
+    s1, s2, *rest = sorted((l, w, h))
+    self.binding_perimeter = 2 * (s1 + s2)
 
   def wrapper(self):
     return (2 * sum(self.areas)) + min(self.areas)
 
+  def ribbon(self):
+    return self.binding_perimeter + self.volume
 
-boxes_ = (Box(*line.rstrip('\n').split('x', maxsplit=3)) for line in sys.stdin)
+boxes_ = [Box(*line.rstrip('\n').split('x', maxsplit=3)) for line in sys.stdin]
 
 # Part 1
 total_wrapper = sum((b.wrapper() for b in boxes_))
 print(total_wrapper)
+
+# Part 2
+total_ribbon = sum((b.ribbon() for b in boxes_))
+print(total_ribbon)
