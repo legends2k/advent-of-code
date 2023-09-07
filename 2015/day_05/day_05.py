0e53cde26907ed3abcac7aa3d7dc19e8e7cae252
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-09-07 10:53:33 +0530

    [2015.5.2] [Python] Count really nice strings
    Missed seeing the "aaaa" case for quite a while; it tripped a
    simple (counter) dict lookup; once realized, quickly solved it by
    keeping a list of occurances solved it.
    
    Fixed Part 1 code in not checking all pairs of a string e.g. in a
    "abcd" the code checked only "ab" and "cd", but not "bc".  Wrote a
    sliding window generator (most satisfying part of the exercise
    for me) for this.

--- 2015/day_05/day_05.py
+++ 2015/day_05/day_05.py
@@ -2,8 +2,15 @@
 
 import sys
 import functools
-import itertools
+from collections import defaultdict
 
+
+def sliding_window(iterable: bytes, window: int):
+  n = len(iterable)
+  if window >= n:
+    yield iterable
+  elif window > 0:
+    yield from (iterable[i:i+window] for i in range(0, len(iterable)-window+1))
 
 def is_nice(s: bytes):
   naughty = (b'ab', b'cd', b'pq', b'xy')
@@ -13,11 +20,31 @@ def is_nice(s: bytes):
   vowel_count = functools.reduce(lambda count, i: count + (i in vowels), s, 0)
   if vowel_count < 3:
     return False
-  for p in itertools.pairwise(s):
+  for p in sliding_window(s, 2):
     if p[0] == p[1]:
       return True
   return False
 
+def is_rule_2_1_complaint(s: bytes):
+  d = defaultdict(list)
+  for i, w in enumerate(sliding_window(s, 2)):
+    occurances = d[w]
+    # handle "aaaa" case; check for any prior non-overlapping occurances
+    if any((j != (i-1) for j in occurances)):
+      return True
+    else:
+      occurances.append(i)
+  return False
+
+def is_rule_2_2_complaint(s: bytes):
+  for i in range(0, len(s)-2):
+    if s[i] == s[i+2]:
+      return True
+  return False
+
+def is_real_nice(s: bytes):
+  return is_rule_2_1_complaint(s) and is_rule_2_2_complaint(s)
+
 words = [line.rstrip('\n').encode() for line in sys.stdin]
-print(len(words))
 print(f'Count of nice strings: {sum((is_nice(w) for w in words))}')
+print(f'Count of really nice strings: {sum((is_real_nice(w) for w in words))}')
