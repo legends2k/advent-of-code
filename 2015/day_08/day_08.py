ca40ad51f2f5cd6332c2e956fc172aac669c010e
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-09-10 19:29:30 +0530

    [2015.8] [Python] String lengths: memory and escaped
    Simple problem, straight-forward solution.  Tried `exec` and realised
    that most escape sequences Python understands (like `\n`, etc.) are
    discounted in the puzzle.
    
    Tried to solve part 2 without reparsing the string by using the number
    of replacements done for part 1 (using `re.subn`) but the `\xHH` case
    disturbs the uniformity.  Went with brute force approach.

---
+++ 2015/day_08/day_08.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+import functools
+
+# Escape backslash though in raw strings as they’ve special meaning in regex
+# e.g. `\s` matches whitespace chars: `\\s` matches a backslash followed by s.
+pattern = re.compile(rb'(\\[\\"])|(\\x[0-9a-fA-F]{2})', flags=re.ASCII)
+
+def mem_len(s: bytes):
+  # subtract two for enclosing quotes
+  return len(re.sub(pattern, b'-', s)) - 2
+
+def enc_len(s: bytes):
+  # add two for new additional quotes
+  return 2+sum(1 if c not in (rb'\"') else 2 for c in s)
+
+code, mem, enc = functools.reduce(
+  lambda total, i: (total[0] + len(i),
+                    total[1] + mem_len(i),
+                    total[2] + enc_len(i)),
+  (line.rstrip('\n').encode() for line in sys.stdin),
+  (0, 0, 0))
+print(f'{code} - {mem} = {code-mem}')
+print(f'{enc} - {code} = {enc-code}')
