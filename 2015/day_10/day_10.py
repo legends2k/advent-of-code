3ea8dcd8460c77ee766579480b3bc49466cdcf56
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-11-07 11:53:46 +0530

    [2015.10.2] [Python] RLE look-and-say
    As iterations grow the elements in the series become exponentially
    large; it didn’t occur to me that memory allocation would be the
    bottleneck and run-length encoding would be great savings.  I cheated
    and looked up some solutions online as I was side-tracked by the
    red-herring that’s Conway’s video [1] as mentioned in the question.
    
    RLE solution computed all 50 iterations in approximately 6.4 seconds
    on the same machine mentioned in the parent commit.
    
    Solution itself is simple; heavily used itertools.
    
    [1]: https://www.youtube.com/watch?v=ea7lJkEhytA

--- 2015/day_10/day_10.py
+++ 2015/day_10/day_10.py
@@ -2,15 +2,28 @@
 
 import sys
 import itertools
+from collections.abc import Iterable
 
+def rle(s: Iterable[any]) -> Iterable[int]:
+  """Returns a generator object of run-length encoded |s|."""
+  # chain.from_iterable() constructor of itertools.chain but is different from
+  # the default chain() constructor; it flattens and chains.
+  return itertools.chain.from_iterable(((len(tuple(ls)), int(c))
+                                        for c, ls in itertools.groupby(s)))
 
-def look_say(s):
-  t = ''
-  for (number, l) in itertools.groupby(s):
-    t = f'{t}{len(tuple(l))}{number}'
-  return t
+def length(it: Iterable[int]) -> int:
+  # This is similar to Rust’s slice::chuncks
+  if sys.version_info.major == 3 and sys.version_info.minor <= 11:
+    return sum(l for l in itertools.islice(it, None, None, 2))
+  return sum((i[0] for i in itertools.batched(it, 2)))
 
 s = sys.stdin.readline().rstrip()
-for _ in range(40):
-  s = look_say(s)
-print(len(s))
+input_ = rle(s)
+for i in range(50):
+  input_ = rle(input_)
+  if i == 39:
+    t = tuple(input_)
+    print(f'Length after 40 iterations: {length(t)}')
+    input_ = t
+
+print(f'Length after 50 iterations: {length(input_)}')
