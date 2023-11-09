d2bdd75f447ea9db8c00053d132d9a73374827aa
Sundaram Ramaswamy <legends2k@yahoo.com>
2023-11-09 06:42:54 +0530

    [2015.12.2] Sum of integers with red filter
    Simple tree flattening.
    
    This solution works for both parts; just remove the filter code to get
    part 1 answer.

--- 2015/day_12/day_12.py
+++ 2015/day_12/day_12.py
@@ -13,7 +13,21 @@ class Counter:
     self.total += i
     return i
 
+def obj_walk(obj: any):
+  to_visit = [obj]
+  while to_visit:
+    o = to_visit.pop()
+    if isinstance(o, list):
+      to_visit.extend(o)
+    elif isinstance(o, dict) and 'red' not in o and 'red' not in o.values():
+      for k, v in o.items():
+        to_visit.append(k)
+        to_visit.append(v)
+    elif isinstance(o, int):
+        yield o
+
 input_ = sys.stdin.readline().rstrip().encode()
 c = Counter()
 j = json.loads(input_, parse_int=lambda s: c.parse(s))
 print(c.total)
+print(sum(obj_walk(j)))
