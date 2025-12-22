5f6f35f608b8cb0dee3a372fc49e9efcc9e172af
Sundaram Ramaswamy <legends2k@yahoo.com>
2025-12-22 11:39:36 +0530

    [2025.6] Drop redundant memory allocations
    Only two vectors: for digits and operations, both from input.

--- 2025/day_06/main.cpp
+++ 2025/day_06/main.cpp
@@ -2,7 +2,6 @@
 #include <cstdint>
 #include <cstdio>
 #include <iostream>
-#include <numeric>
 #include <print>
 #include <ranges>
 #include <string>
@@ -19,18 +18,18 @@ struct Offset {
   size_t right;
 };
 
-std::vector<uint64_t>
-get_field(const std::vector<char>& table,
-          const std::vector<std::pair<Operation, Offset>>& ops,
-          size_t columns,
-          size_t f) {
+uint64_t
+reduce_field(const std::vector<char>& table,
+             const std::vector<std::pair<Operation, Offset>>& ops,
+             size_t columns,
+             size_t f) {
   // Read the last column for field width.
   const auto rows = table.size() / columns;
   const auto& left = ops[f].second.left;
   const auto& right = ops[f].second.right;
   const auto width = right - left;
-  std::vector<uint64_t> numbers;
-  numbers.reserve(rows);
+  const auto oid = ops[f].first;
+  uint64_t result = (oid == Operation::Multiply) ? 1 : 0;
   for (auto i = 0u; i < rows; ++i) {
     auto offset = i * columns + left;
     const auto end = table.data() + offset + width;
@@ -40,23 +39,23 @@ get_field(const std::vector<char>& table,
     uint64_t number = 0;
     // TODO: check return value.
     std::from_chars(&table[offset], end, number);
-    numbers.push_back(number);
+    result =
+      (oid == Operation::Multiply) ? (result * number) : (result + number);
   }
-  return std::move(numbers);
+  return result;
 }
 
-std::vector<uint64_t>
-get_field_transpose(const std::vector<char>& table,
-                    const std::vector<std::pair<Operation, Offset>>& ops,
-                    size_t columns,
-                    size_t f) {
+uint64_t
+reduce_field_transpose(const std::vector<char>& table,
+                       const std::vector<std::pair<Operation, Offset>>& ops,
+                       size_t columns,
+                       size_t f) {
   // Read the last column for field width.
   const auto rows = table.size() / columns;
   const auto& left = ops[f].second.left;
   const auto& right = ops[f].second.right;
-  const auto width = right - left;
-  std::vector<uint64_t> numbers;
-  numbers.reserve(width);
+  const auto oid = ops[f].first;
+  uint64_t result = (oid == Operation::Multiply) ? 1 : 0;
   std::string num;
   num.reserve(10);
   for (auto c = left; c < right; ++c) {
@@ -64,9 +63,11 @@ get_field_transpose(const std::vector<char>& table,
     for (auto r = 0u; r < rows; ++r)
       if (auto ch  = table[r * columns + c]; ch != ' ')
         num.push_back(ch);
-    numbers.push_back(std::stoull(num));
+    const auto number = std::stoull(num);
+    result =
+      (oid == Operation::Multiply) ? (result * number) : (result + number);
   }
-  return std::move(numbers);
+  return result;
 }
 
 int main() {
@@ -76,7 +77,7 @@ int main() {
   std::string line;
   std::getline(std::cin, line);
   const uint16_t columns = line.size();
-  // Store numbers column-wise.
+  // 1D digit buffer sized rows × columns.
   std::vector<char> table;
   // Preallocate assuming 10 lines.
   table.reserve(columns * 10u);
@@ -90,7 +91,9 @@ int main() {
   }
   const auto size_sans_ops = table.size() - columns;
   const auto rows = (table.size() / columns) - 1u;
+  // “Fields” are columns of numbers (as opposed to columns of digits).
   std::vector<std::pair<Operation, Offset>> ops;
+  ops.reserve(columns);
   for (auto i = (rows * columns); i < table.size(); ++i) {
     if (table[i] != ' ')
       ops.push_back(
@@ -106,35 +109,13 @@ int main() {
 
   // Part 1
   uint64_t result1 = 0u;
-  for (auto i = 0u; i < ops.size(); ++i) {
-    const auto numbers = get_field(table, ops, columns, i);
-    if (ops[i].first == Operation::Multiply)
-      result1 += std::accumulate(numbers.cbegin(),
-                                 numbers.cend(),
-                                 uint64_t(1u),
-                                 [](auto b, auto i) { return b * i; });
-    else
-      result1 += std::accumulate(numbers.cbegin(),
-                                 numbers.cend(),
-                                 uint64_t(0u),
-                                 [](auto b, auto i) { return b + i; });
-  }
+  for (auto i = 0u; i < ops.size(); ++i)
+    result1 += reduce_field(table, ops, columns, i);
   std::println("Total: {}", result1);
 
   // Part 2: transpose
   uint64_t result2 = 0u;
-  for (auto i = 0u; i < ops.size(); ++i) {
-    const auto numbers = get_field_transpose(table, ops, columns, i);
-    if (ops[i].first == Operation::Multiply)
-      result2 += std::accumulate(numbers.cbegin(),
-                                 numbers.cend(),
-                                 uint64_t(1u),
-                                 [](auto b, auto i) { return b * i; });
-    else
-      result2 += std::accumulate(numbers.cbegin(),
-                                 numbers.cend(),
-                                 uint64_t(0u),
-                                 [](auto b, auto i) { return b + i; });
-  }
+  for (auto i = 0u; i < ops.size(); ++i)
+    result2 += reduce_field_transpose(table, ops, columns, i);
   std::println("Total Transposed: {}", result2);
 }
