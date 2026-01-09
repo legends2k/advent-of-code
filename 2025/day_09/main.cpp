19836fd22be42c01e935f908ae153bf0018dd05f
Sundaram Ramaswamy <legends2k@yahoo.com>
2026-01-09 10:14:13 +0530

    [2025.9.2] Attempt 1
    This is one of the hardest problems to solve for me.  I got misled by
    the puzzle’s wording (this isn’t new, day 8 had elements of this too).
    I over-read and follow the instruction to the dot while I should just
    understand what he _means_.
    
    Solution itself is incorrect because not adjacent red tiles but _any_
    two red tiles form the solution rectangle.  Will fix in the follow-up
    commit.

--- 2025/day_09/main.cpp
+++ 2025/day_09/main.cpp
@@ -6,6 +6,8 @@
 #include <print>
 #include <string>
 #include <string_view>
+#include <unordered_map>
+#include <utility>
 #include <vector>
 
 #ifndef NDEBUG
@@ -16,6 +18,8 @@ struct Point {
   float x = std::numeric_limits<float>::infinity();
   float y = std::numeric_limits<float>::infinity();
 
+  Point() = default;
+  Point(float a, float b) : x{a}, y{b} {};
   Point(std::string_view s) {
     const auto i = s.find(",");
     const auto p = s.substr(0, i);
@@ -25,11 +29,20 @@ struct Point {
   }
 };
 
+bool operator==(Point a, Point b) {
+  return (a.x == b.x) && (a.y == b.y);
+}
+
 struct Rect {
   Point p[2];
+
   float area() const {
     return (1.0f + std::abs(p[0].x - p[1].x)) *
       (1.0f + std::abs(p[0].y - p[1].y));
+  }
+
+  auto others() const {
+    return std::pair{Point{p[0].x, p[1].y}, Point{p[1].x, p[0].y}};
   }
 };
 
@@ -51,6 +64,48 @@ struct std::formatter<Point> {
 
 #endif // NDEBUG
 
+struct Line {
+  float left = std::numeric_limits<float>::infinity();
+  float right = -std::numeric_limits<float>::infinity();
+
+  bool contains_point(float x) const {
+    return (left <= x) && (right >= x);
+  }
+};
+
+struct Polygon {
+
+  Polygon(const std::vector<Point> points) {
+    const auto N = points.size();
+    bounds.reserve(N);
+    Point prev;
+    prev = points.front();
+    auto delta = 0.0f;
+    for (auto i = 0; i <= N; ++i) {
+      auto cur = points[i % N];
+      delta = (cur.y > prev.y) ? 1.0f : ((cur.y < prev.y) ? -1.0f : 0.0f);
+      for (auto y = prev.y + delta; y != cur.y; y += delta)
+        set(Point{cur.x, y});
+      set(cur);
+      prev = cur;
+    }
+  }
+
+  bool is_inside(Point p) const {
+    const auto it = bounds.find(p.y);
+    return (it != bounds.cend()) ? it->second.contains_point(p.x) : false;
+  }
+
+private:
+  void set(Point p) {
+    auto& line = bounds[p.y];
+    line.left = std::min(p.x, line.left);
+    line.right = std::max(p.x, line.right);
+  }
+
+  std::unordered_map<float, Line> bounds;
+};
+
 int main() {
   std::ios::sync_with_stdio(false);
   std::cin.tie(nullptr);
@@ -59,13 +114,42 @@ int main() {
   std::string line;
   while (std::getline(std::cin, line))
     reds.emplace_back(line);
+  const auto N = reds.size();
 
+  // Part 1
   float max_area = -std::numeric_limits<float>::infinity();
-  for (auto i = 0u; i < reds.size(); ++i) {
+  for (auto i = 0u; i < N; ++i) {
     for (auto j = i + 1; j < reds.size(); ++j) {
       const float area = Rect{reds[i], reds[j]}.area();
       max_area = std::max(max_area, area);
     }
   }
   std::println("Maximum area rectangle formed with red tiles: {}", max_area);
+
+  Polygon p(reds);
+
+  // A local solution -- assuming if three points are ‘in’ then just testing
+  // fourth -- won’t work.  This figure can grow either way: encompassing ? or
+  // leaving it out.  Land inlets is the Achilles heel of a local solution.
+  //
+  //   |
+  //   +------O   ?
+  //          |   +---
+  //          |   |
+  //          O---O
+  //
+
+  // Iterate every point with 2 preceding and trailing ones.
+  float max_area_within = -std::numeric_limits<float>::infinity();
+  for (auto i = 0u; i < N; ++i) {
+    const auto p1 = reds[i];
+    const auto p2 = reds[(i + 1) % N];
+    const auto p3 = reds[(i + 2) % N];
+    const auto r = Rect{p1, p3};
+    auto [p41, p42] = r.others();
+    if (p.is_inside((p41 == p2) ? p42 : p41))
+      max_area_within = std::max(max_area_within, r.area());
+  }
+  std::println("Maximum area rectangle formed by red-green tiles: {}",
+               max_area_within);
 }
